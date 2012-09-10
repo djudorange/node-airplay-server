5d0296af41e4d9d283a61f7bf94f4ed3d3b7150a
Joe Ferner <joe@fernsroth.com>
2012-09-10 12:19:51 -0400

    more progress

--- bin/airplay-server.js
+++ bin/airplay-server.js
@@ -8,4 +8,13 @@ var server = airplay.createServer();
 server.on('connection', function(client) {
   console.log(client);
 });
+server.on('play', function(data) {
+  console.log('play', data);
+});
+server.on('stop', function(data) {
+  console.log('stop', data);
+});
+server.on('rate', function(data) {
+  console.log('rate', data);
+});
 server.start();
