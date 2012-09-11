ed3b18ff814bb6ce7ca2428c35cb247adaf2e364
Joe Ferner <joe@fernsroth.com>
2012-09-11 10:45:50 -0400

    working ptth

--- bin/airplay-server.js
+++ bin/airplay-server.js
@@ -5,16 +5,33 @@
 var airplay = require('../');
 
 var server = airplay.createServer();
-server.on('connection', function(client) {
-  console.log(client);
+
+server.on('error', function(error) {
+  console.log('error', error);
 });
+
 server.on('play', function(data) {
   console.log('play', data);
+  var body =
+    '<?xml version="1.0" encoding="UTF-8"?>\n'
+      + '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">\n'
+      + '<plist version="1.0">\n'
+      + '<dict>\n'
+      + '\t<key>category</key>\n'
+      + '\t<string>video</string>\n'
+      + '\t<key>state</key>\n'
+      + '\t<string>loading</string>\n'
+      + '</dict>\n'
+      + '</plist>\n';
+  server.sendEvent(body);
 });
+
 server.on('stop', function(data) {
   console.log('stop', data);
 });
+
 server.on('rate', function(data) {
   console.log('rate', data);
 });
+
 server.start();
